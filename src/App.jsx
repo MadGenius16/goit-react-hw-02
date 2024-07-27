11281b1bf42a6e20cf14f3213ccf7870e01690cd
MadGenius16 <aleksandrco777@gmail.com>
2024-07-27 12:33:48 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-      
+      <h1>Sip Happens Café</h1>
+      <p>Please leave your feedback about our service by selecting one of the options below.</p>
     </>
   )
 }
